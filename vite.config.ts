12cd3e97b26a7b611b23e56d9c46bedb8f41d33e
steven pousty <steve.pousty@gmail.com>
2025-08-29 21:36:12 -0700

    Updates to deploy to openshift

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,12 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 3000,
         strictPort: true,
-        allowedHosts: 'all',
+        allowedHosts: true,
     },
     preview: {
         host: '0.0.0.0',
         port: 3000,
         strictPort: true,
-        allowedHosts: 'all',
+        allowedHosts: true,
     },
 })
